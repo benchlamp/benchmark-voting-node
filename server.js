4068b63947200cf37f9ca5142e3c9c8466fd6cec
James <jamesrea83@gmail.com>
2017-07-19 15:40:53 +0000

    changed font size on create button; tidying on create page

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // server.js
 
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-var path = require("path");
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var express         = require('express');
+var app             = express();
+var port            = process.env.PORT || 8080;
+var mongoose        = require('mongoose');
+var passport        = require('passport');
+var flash           = require('connect-flash');
+var path            = require("path");
+var morgan          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var session         = require('express-session');
 
 //var configDB = require('./config/database.js'); //(Cloud 9)
 //mongoose.connect(configDB.url); // connect to our database (Cloud 9)
