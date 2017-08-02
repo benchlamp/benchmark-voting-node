f056f18f6d69fe82c7ffc46216134ac05e4ee707
James <jamesrea83@gmail.com>
2017-08-02 15:00:31 +0000

    profile delete buttons sending survey id

--- app/controllers/profile.client.js
+++ app/controllers/profile.client.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
     
-    console.log("surveys.client.js running");
+    console.log("data length = " + data.length);
+    
+
+    
+    if (data.length > 0) {
+        $("#profile-survey-list-intro").append("<p>Here's a list of the surveys you've created</p>");
+    } else {
+        $("#profile-survey-list-intro").append("<p>It doesn't look like you've created any surveys yet</p>");
+    }
+    
     
     data.forEach(function(survey) {
-        console.log(survey);
         $("#survey-list").append(
             "<div class='list-group-item list-group-item-action profile-list-group-item'>" +
                 "<a href='/display?id=" + survey._id + "' class='btn profile-survey-link'>" + survey.name + "</a>" +
-                "<a class='btn btn-danger profile-delete-link'><span class='glyphicon glyphicon-trash'></span></a>" +
+                "<button id='" + survey._id + "' class='btn btn-danger profile-delete-link glyphicon glyphicon-trash' style='text-align:center' title='Delete survey'></button>" +
             
             "</div>"
-            
-            //<a href='/display?id=" + survey._id + "' >" + survey.name + "</a><button>Delete</button></div>"
+
             )
+    })
+    
+
+    $(".profile-delete-link").on("click", function(event) {
+      confirm("Are you sure you want to delete survey id " + event.target.id);
     })
     
 })
